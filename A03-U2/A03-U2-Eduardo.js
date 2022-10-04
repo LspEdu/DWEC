eabc361b5dda270d76310f99e913b4fdc8ae8e06
Eduardo <eduardolmsan@gmail.com>
2022-10-04 19:07:04 +0200

    Darle una vuelta a esto

--- A03-U2/A03-U2-Eduardo.js
+++ A03-U2/A03-U2-Eduardo.js
@@ -1,10 +1,22 @@
 
-function mult()
-{
+function mult() { 
     var num1 = document.getElementById("num1").value;
     var num2 = document.getElementById("num2").value;
+    var flag = 0;
+    if (num1 < num2) {
+        for (var divi = num2; divi % 8 == 0; divi - 8) {
+            console.log("si 1");
+            document.getElementById("respond").innerText += divi;
+        }
+    } else if (num2 < num1) {
+        console.log("si 2");
+        for (var divi = num1; divi - 8 >= 8; divi - 8) {
+           
+            document.getElementById("respond").innerText += divi;
+        }
+    } else if (num1 == num2) {
+      
+        document.getElementById("respond").innerText = "Son iguales";
 
-    for ( ; ; ){
-        
     }
 }
