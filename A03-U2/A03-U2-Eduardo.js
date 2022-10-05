ec5eceafdb0f8d58df866beb4e40e3b072331769
Eduardo <eduardolmsan@gmail.com>
2022-10-05 15:52:29 +0200

    El 3 hecho

--- A03-U2/A03-U2-Eduardo.js
+++ A03-U2/A03-U2-Eduardo.js
@@ -1,22 +1,23 @@
 
 function mult() { 
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
+    document.getElementById("respond").innerHTML="";
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
     var flag = 0;
-    if (num1 < num2) {
-        for (var divi = num2; divi % 8 == 0; divi - 8) {
-            console.log("si 1");
-            document.getElementById("respond").innerText += divi;
-        }
-    } else if (num2 < num1) {
-        console.log("si 2");
-        for (var divi = num1; divi - 8 >= 8; divi - 8) {
-           
-            document.getElementById("respond").innerText += divi;
-        }
-    } else if (num1 == num2) {
-      
-        document.getElementById("respond").innerText = "Son iguales";
 
-    }
+    if (num1 < num2){
+        for (var d = num1 ; d <= num2  ; d++){
+            var x = d / 8;
+            if (x % 1 === 0){
+                document.getElementById("respond").innerText += "  "  + d;
+            }
+        }
+    } else if( num1 > num2 ) {
+        for (var d = num2 ; d <= num1 ; d++){
+            var x = d / 8;
+            if (x % 1 === 0){
+                document.getElementById("respond").innerText += "  "  + d;
+            }
+        }
+    } else if( num1 == num2){ document.getElementById("respond").innerText = "Error: los números son iguales";}
 }
