a81ae94f494919250d3b59ef73504adf4aa308a5
edu <edu@PortatilDebian.home>
2022-11-07 21:21:03 +0100

    Preparacion TODO// Validar y Programa

---
+++ U4/U4-A01-Eduardo.js
@@ -0,0 +1,13 @@
+function crear_precio(){
+    var num = document.getElementById("num").value;
+    var iva = document.querySelector('input[name="IVA"]:checked') 
+                ? document.querySelector('input[name="IVA"]:checked').value  
+                :21;
+
+    if(num != ''){
+
+    }
+    else{
+        
+    }
+}
