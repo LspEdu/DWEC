28060ef9c5ecce07335e34079dc29a90dad1c385
edu <edu@PortatilDebian.home>
2022-11-08 17:13:17 +0100

    U4-A1 Acabada

--- U4/U4-A01-Eduardo.js
+++ U4/U4-A01-Eduardo.js
@@ -1,13 +1,29 @@
 function crear_precio(){
-    var num = document.getElementById("num").value;
+    var num = document.getElementById("num");
     var iva = document.querySelector('input[name="IVA"]:checked') 
                 ? document.querySelector('input[name="IVA"]:checked').value  
                 :21;
+    var divi = document.getElementById("divi");
 
-    if(num != ''){
-
+    if(num.value != ''){
+        style_correcto(num,divi);
+        let h1 = document.getElementById("h1");
+        let resul = ((num.value*iva)/100)+parseInt(num.value);
+        h1.innerText=resul;
+        console.log(resul)
     }
     else{
-        
+        alert("Debes de completar el Precio");
+        style_error(num,divi);
     }
+}
+function style_correcto(num, divi){
+    num.style="border-color: black";
+    divi.style="display:flex";
+    
+}
+function style_error(num, divi){
+        num.style="border-color: red";
+        divi.style="display:none";
+        
 }
